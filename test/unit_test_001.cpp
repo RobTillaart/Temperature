36b75f28c509f4d32f414c9f02fc95dbeb4ffc52
rob tillaart <rob.tillaart@gmail.com>
2021-01-08 23:00:39 +0100

    fix

--- test/unit_test_001.cpp
+++ test/unit_test_001.cpp
@@ -58,27 +58,28 @@ unittest(test_conversion)
 
 unittest(test_dewpoint)
 {
-  assertEqualFloat(0, dewPoint(20, 50), 0.0001);
-  assertEqualFloat(0, dewPointFast(20, 50), 0.0001);
-  assertEqualFloat(0, humidex(20, 10),  0.0001);
+  assertEqualFloat(9.27985, dewPoint(20, 50), 0.0001);
+  assertEqualFloat(9.25489, dewPointFast(20, 50), 0.0001);
+  assertEqualFloat(21.2829, humidex(20, 10),  0.0001);
 }
 
 
 unittest(test_heatIndex)
 {
-  assertEqualFloat(0, heatIndex(20, 50), 0.0001);
-  assertEqualFloat(0, heatIndexC(20, 50), 0.0001);
+  assertEqualFloat(206.46,  heatIndex(20, 50), 0.0001);
+  assertEqualFloat(0,       heatIndex(68, 50), 0.0001);
+  assertEqualFloat(25.1949, heatIndexC(20, 50), 0.0001);
 }
 
 
 unittest(test_windChill)
 {
-  assertEqualFloat(0, WindChill_F_mph (100, 10, true), 0.0001);
-  assertEqualFloat(0, WindChill_C_kmph(37, 10, true),  0.0001);
-  assertEqualFloat(0, WindChill_C_mps (37, 10, true),  0.0001);
-  assertEqualFloat(0, WindChill_F_mph (100, 10, false), 0.0001);
-  assertEqualFloat(0, WindChill_C_kmph(37, 10, false),  0.0001);
-  assertEqualFloat(0, WindChill_C_mps (37, 10, false),  0.0001);
+  assertEqualFloat(107.108, WindChill_F_mph (100, 10, true), 0.0001);
+  assertEqualFloat(40.8862, WindChill_C_kmph(37, 10, true),  0.0001);
+  assertEqualFloat(41.9713, WindChill_C_mps (37, 10, true),  0.0001);
+  assertEqualFloat(166.99,  WindChill_F_mph (100, 10, false), 0.0001);
+  assertEqualFloat(69.1205, WindChill_C_kmph(37, 10, false),  0.0001);
+  assertEqualFloat(154.934, WindChill_C_mps (37, 10, false),  0.0001);
 }
 
 
