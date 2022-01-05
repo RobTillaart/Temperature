720333b566728c5ff13bfba7a3acef4971b0852d
rob tillaart <rob.tillaart@gmail.com>
2022-01-05 21:04:29 +0100

    fix

--- test/unit_test_001.cpp
+++ test/unit_test_001.cpp
@@ -67,6 +67,9 @@ unittest(test_dewpoint)
 
 unittest(test_heatIndex)
 {
+  fprintf(stderr, "%f\n", heatIndex(20, 50));
+  fprintf(stderr, "%f\n", heatIndex(68, 50));
+  fprintf(stderr, "%f\n", heatIndexC(20, 50));
   assertEqualFloat(206.46,  heatIndex(20, 50), 0.001);
   assertEqualFloat(77.3509, heatIndex(68, 50), 0.001);
   assertEqualFloat(25.1949, heatIndexC(20, 50), 0.001);
